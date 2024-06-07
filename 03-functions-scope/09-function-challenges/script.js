ddb33329b4f44221ffb951d60afa842f89e87f80
iykechuks11 <johnchamp1111@gmail.com>
2024-06-07 10:06:11 +0300

    Updated learning with functions

---
+++ 03-functions-scope/09-function-challenges/script.js
@@ -0,0 +1,16 @@
+// Challenge 1
+
+const getCelsius = (f) => {
+  const celsius = ((f - 32) * 5) / 9;
+  return celsius;
+};
+
+console.log(getCelsius(32));
+
+// Challenge 2
+
+const minMax = (...numbers) => {
+  return Math.max();
+};
+
+console.log(minMax(1, 2, 3, 4, 5));
