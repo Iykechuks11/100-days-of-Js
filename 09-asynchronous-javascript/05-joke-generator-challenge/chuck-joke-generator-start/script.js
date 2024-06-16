e65870b49ae1a04058f7e94ca3074c77cc15e4a4
iykechuks11 <johnchamp1111@gmail.com>
2024-06-16 23:41:58 +0300

    Project chock norris api joke generator completed

---
+++ 09-asynchronous-javascript/05-joke-generator-challenge/chuck-joke-generator-start/script.js
@@ -0,0 +1,18 @@
+const btn = document.getElementById('joke-btn');
+const jokeEl = document.getElementById('joke');
+
+function joke() {
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://api.chucknorris.io/jokes/random');
+  xhr.onreadystatechange = function () {
+    if (this.status == 200 && this.readyState == 4) {
+      const data = JSON.parse(this.responseText);
+      jokeEl.innerHTML = data.value;
+    } else {
+      jokeEl.innerHTML = '404 \n No jokes found!';
+    }
+  };
+  xhr.send();
+}
+
+btn.addEventListener('click', joke);
