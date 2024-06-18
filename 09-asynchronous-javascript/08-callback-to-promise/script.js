8c6cd27e3e76ae379890360bd4ee85b74bffb276
iykechuks11 <johnchamp1111@gmail.com>
2024-06-18 10:58:10 +0300

    learning

--- 09-asynchronous-javascript/08-callback-to-promise/script.js
+++ 09-asynchronous-javascript/08-callback-to-promise/script.js
@@ -1,17 +1,9 @@
-
 const posts = [
   { title: 'Post One', body: 'This is post one' },
   { title: 'Post Two', body: 'This is post two' },
 ];
 
-function createPost(post, cb) {
-  setTimeout(() => {
-    posts.push(post);
-    cb();
-  }, 2000);
-}
-
-function getPosts() {
+const getPosts = () => {
   setTimeout(() => {
     posts.forEach(function (post) {
       const div = document.createElement('div');
@@ -19,6 +11,38 @@ function getPosts() {
       document.querySelector('#posts').appendChild(div);
     });
   }, 1000);
+};
+
+// getPosts.then((post) => {
+//   console.log(post);
+// });
+
+console.log('Posts loading...');
+
+function createPost(post) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      let error = true;
+
+      if (!error) {
+        posts.push(post);
+      } else {
+        reject('Something went wrong.');
+      }
+      // posts.push(post);
+      resolve();
+    }, 2000);
+  });
 }
 
-createPost({ title: 'Post Three', body: 'This is post' }, getPosts);
+function showError(error) {
+  const h3 = document.createElement('h3');
+  h3.innerHTML = `<strong>${error}</strong>`;
+  h3.style.color = 'red';
+  document.getElementById('posts').appendChild(h3);
+}
+
+createPost({ title: 'Post Three', body: 'This is post' })
+  .then(getPosts)
+  .catch(showError);
+// getPosts().then(createPost);
